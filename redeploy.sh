43ac5fe51af451a6daed41147d0e3b42b1349895
Eike Thies <thies.eike@gmail.com>
2021-08-14 22:34:32 +0200

    raspberry config

--- redeploy.sh
+++ redeploy.sh
@@ -2,28 +2,28 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-TARGET="/home/brawauser/brawa/repo"
+TARGET="/home/pi/brawa/repo"
 BRANCH="release"
 
 # Redirect stdout/stderr to a file
 DS=`date +%Y-%m-%d`;
-exec > /home/brawauser/brawa/webhook_output.log 2>&1
+exec > /home/pi/brawa/webhook_output.log 2>&1
 
 # only checking out the master (or whatever branch you would like to deploy)
 #if [ "$ref" = "refs/heads/$BRANCH" ];
 #then
     echo "Branch release received. Deploying ${BRANCH} branch ..."
-    cd /home/brawauser/brawa/repo/ &&
+    cd /home/pi/brawa/repo/ &&
     git pull
 
     echo "Stopping All services" &&
-    /home/brawauser/.nvm/versions/node/v16.6.1/bin/pm2 stop all
+    /home/pi/.nvm/versions/node/v16.6.2/bin/pm2 stop all
 
     echo "Building Backend" &&
-    cd /home/brawauser/brawa/repo/backend && npm run build
+    cd /home/pi/brawa/repo/backend && npm run build
 
     echo "Start Backend Service again" &&
-    /home/brawauser/.nvm/versions/node/v16.6.1/bin/pm2 start backend
+    /home/pi/.nvm/versions/node/v16.6.2/bin/pm2 start backend
 
     echo "Start Webserver again" &&
    # sudo caddy start --config /etc/caddy/Caddyfile
